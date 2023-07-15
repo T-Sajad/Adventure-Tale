1b8faa1ce42b860968a23b711044aa8f9a22c7cf
T-Sajad <talalsajad300@gmail.com>
2023-07-15 13:23:54 +0100

    part 4.8.1

--- src/parser.js
+++ src/parser.js
@@ -3,38 +3,31 @@ const LINE_BREAK_PATTERN = "\n"
 
 function parse (source){
     const storyJson = []
-
     // split the source string into lines
     const x = source.split(LINE_BREAK_PATTERN)
     console.log(x)
     let page = undefined;
+    
     for(const line of x){
         // if the line starts with = 
         if(line.startsWith('=')){
             // parse a new story id
-
             // create a new page object
-
             page ={id:line.substring(1)}
             // add it to the storyJson array
             storyJson.push(page)
         }
         else if(line.startsWith('*')){
           // parse a link
-        } else if (line) {
+        } 
+        else if (line) {
           // parse content
-          
           const targetPage = page;
-
           //we need to add the content to the page 
           targetPage.content = line
         }
     }
     return storyJson
-
 }
-
-
-
 
 export default parse
