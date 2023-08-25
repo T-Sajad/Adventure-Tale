4569bffe6214efdeecc93c10edaea68dfb2cc9ff
T-Sajad <talalsajad300@gmail.com>
2023-08-25 18:47:17 +0100

    cli part 2

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,8 @@
 import { readFileSync, writeFileSync } from 'node:fs';
 import parse  from './parser.js'
+
 // read input file in
-//TOD0: write conditional check if the file is not markdown.
-
-
-
-
+//TODO: write conditional check if the file is not markdown.
 const path = process.argv[2]
 console.log('reading input from: ', path)
 
@@ -14,6 +11,21 @@ const storyMarkdown = readFileSync(path,'utf8')
 // parse file to json
 const storyJson = parse(storyMarkdown)
 
-// write file
-writeFileSync('./story.json', JSON.stringify(storyJson, null, 2))
+//what file path do i need to write to?
+// 1) look at the input path
+// 2) conert the .md file extension to a .json file extension
 
+const outputPath =  path.replace('.md','.json')
+
+// write file to disk
+writeFileSync(outputPath, JSON.stringify(storyJson, null, 2))
+
+
+// npm parse /tmp/my-story.md
+// input /tmp/my-story.md
+// output /tmp/my-story.json
+
+
+// npm parse src/data/myStory.md
+// input src/data/myStory.md
+// output src/data/myStory.json
